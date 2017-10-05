90323782712eca4a0251931f6291835896ec00e4
sirelint <sirelin.tallo@gmail.com>
2017-10-05 12:08:44 +0300

    First commit

---
+++ js/game.js
@@ -0,0 +1,92 @@
+'use strict';
+
+function gid(x) {
+    return document.getElementById(x);
+}
+
+function myFun(x) {
+    var y = "Siin on uus sisu ja " + x;
+    console.log("Vajutas nuppu");
+    gid("koht1").innerHTML = y;
+}
+
+function press(x, y) {
+    //console.log(x+","+y);
+}
+
+function makeBoard(size, bombs) {
+    var board = [];
+    
+    //console.log("makeboard");
+  
+    if (bombs >= size * size) {
+        throw "too many bombs for this size";
+    }
+    // initialize board, filling with zeros
+    for (var x=0; x<size; x++) {
+        board[x]=[]; // insert empty subarray
+    for (var y=0; y<size; y++) board[x][y]=0;
+}
+
+    // now fill board with bombs in random positions
+    var i=bombs;
+    while (i>0) {
+        // generate random x and y in range 0...size-1
+        x=Math.floor(Math.random() * size);
+        y=Math.floor(Math.random() * size);
+        // put bomb on x,y unless there is a bomb already
+        if (board[x][y]!=1) {
+            board[x][y]=1;
+            i--; // bomb successfully positioned, one less to go
+            console.log("positioned "+x+", "+y+" yet to go "+i);
+        }
+    }
+    return board;
+    console.log("tere");
+    
+}
+function drawBoard(size) {
+    var c="";
+    var board = makeBoard(size,5);
+    //console.log(board);
+    
+    c="<table>";
+    for(var x=0; x<board.length; x++) {
+        c+="<tr>";
+        for(var y=0; y<board.length; y++) {
+            c+="<td onclick='press("+x+", "+y+")'>x</td>";
+        }
+        c+="</tr>";
+    }
+    
+    c+="</table>";
+    //console.log(c);
+    gid("koht1").innerHTML = c;
+    //$("#koht1").html(c);
+}
+function neighbours(size,x,y) {
+  var list=[];
+  for (var i=-1; i<=1; i++) {    
+    for (var j=-1; j<=1; j++) {
+      // square is not a neighbour of itself
+      if (i==0 && j==0) continue;
+      // check whether the the neighbour is inside board bounds
+      if ((x+i)>=0 && (x+i)<size && (y+j)>=0 && (y+j)<size) {
+        list.push([x+i,y+j]);  
+      }
+    }
+  }
+  return list;
+}  
+
+function startGame() {
+    var s,v;
+    console.log("startGame");
+    s=gid("sizeselect");
+    console.log("index: "+s.selectedIndex);
+    v=s.options[s.selectedIndex].value;
+    console.log(v);
+    
+    var b=makeBoard(parseInt(v),2);
+    drawBoard(b);
+}
